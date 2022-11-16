6dc4e4fb18253920b7fd95de6bb969d049dd922e
Usuario <Usuario@lapAnayansi>
2022-11-16 11:42:35 -0700

    Segundo Repositorio

---
+++ src/ejemplogithubu5/EjemploGitHubU5.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplogithubu5;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EjemploGitHubU5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("AnyMeraz"); 
+        System.out.println("OmarILG"); 
+        System.out.println("Saladitas");
+    }
+    
+}
